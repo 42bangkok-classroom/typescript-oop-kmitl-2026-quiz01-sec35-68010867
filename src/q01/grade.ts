509a9503d59d925696f865fa1cb20b3b664af87e
Phasawut Imsurman <68010867@kmitl.ac.th>
2025-12-15 10:16:48 +0700

    test1

--- src/q01/grade.ts
+++ src/q01/grade.ts
@@ -1,42 +1,44 @@
 if (process.argv.length < 3) {
-    console.error("Error: Please provide a single number as a command-line argument.");
+    console.error("Invalid Input");
     process.exit(1);
 }
-let num = parseInt(process.argv[3]);
-if(num <0 || num > 100) process.exit(1);
+let num = parseInt(process.argv[2]);
+
+if(num <0 || num > 100){
+    console.error("Invalid Input");
+    process.exit(1);
+} 
 switch(num) { 
    case 1: { 
     if(num>=80){ 
-        console.log("Grade A");
+        console.log("Grade is A");
     }
-
-      break; 
+     break; 
    } 
    case 2: { 
     if(num>=70){ 
-        console.log("Grade B");
+        console.log("Grade is B");
     }
       break; 
    } 
    case 3: { 
     if(num>=60){ 
-        console.log("Grade C");
+        console.log("Grade is C");
     }
 
       break; 
    } 
    case 4: { 
     if(num>=50){ 
-        console.log("Grade D");
+        console.log("Grade is D");
     }
 
       break; 
    } 
    case 5: { 
     if(num<50){ 
-        console.log("Grade F");
+        console.log("Grade is E");
     }
-
       break; 
    } 
    
